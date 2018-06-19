f0457bcb3a2f8805a8957bdcdcc852c94141f79d
Abraham Anderson <abeis@msn.com>
2018-06-19 17:25:08 -0500

    setup base html structure and displaying the info

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var exphbs  = require('express-handlebars');
 var bodyParser = require("body-parser");
 const cheerio = require('cheerio')
 var request = require("request");
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
  
 var app = express();
 
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapedData";
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapedData";
 
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+//mongoose.Promise = Promise;
+//mongoose.connect(MONGODB_URI);
 
  
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -19,31 +19,25 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '../public'));
  
 app.get('/', function (req, res) {
-    res.render('home');
-});
+    //res.render('home');
 
-request("https://lifehacker.com/tag/programming", function(error, response, html) {
 
-  
-  var $ = cheerio.load(html);
-
-  
-  var results = [];
-
-  $("div .js_post-wrapper").each(function(i, element) {
-
-    
-    var title = $(element).find(".js_entry-link").text();
-    var link = $(element).find(".js_entry-link").attr("href");
-    var summury = $(element).find(".entry-summary").text();
-
-    results.push({
-      title: title,
-      link: link,
-      summury: summury
+  request("https://lifehacker.com/tag/programming", function(error, response, html) {
+    var $ = cheerio.load(html);
+    var results = [];
+    $("div .js_post-wrapper").each(function(i, element) {
+      var title = $(element).find(".js_entry-link").text();
+      var link = $(element).find(".js_entry-link").attr("href");
+      var summary = $(element).find(".entry-summary").text();
+      results.push({
+        title: title,
+        link: link,
+        summary: summary
+      });
     });
-  });
-  console.log(results[0]);
+    res.render('home', {"news" : results})
+  })
+
 });
- 
+
 app.listen(3000);
