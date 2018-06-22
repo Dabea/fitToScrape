7272a269a3dea94a9bd9282f02caa40390c33d2e
Abraham Anderson <abeis@msn.com>
2018-06-22 17:35:58 -0500

    added remove saved story button and funcions

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,8 @@
  
+ $('.delete-story').on('click', (event) => removeSavedStory(event)   
+ )
+ 
+ function removeSavedStory(event) {
+     console.log(event);
+    $(event.target).closest('.article-js').remove();
+ }
